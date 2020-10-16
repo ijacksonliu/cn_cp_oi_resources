42ceafb9a40aff1d45212409c977dc44d7c23579
Jackson Liu <2641800104@qq.com>
2020-10-16 17:21:39 +0800

    Update mytemplate.cpp

--- mytemplate.cpp
+++ mytemplate.cpp
@@ -3,7 +3,11 @@
 #define fi first;
 #define se second;
 #define INTFOR(i,a,b) for(int i=(a);i<(b);++i)
+#define EQFOR(i,a,b) for(int i=(a);i<=(b);++i)
 #define LLFOR(i,a,b) for(long long int i=(a);i<(b);++i)
+#define EQLLFOR(i,a,b) for(long long int i=(a);i<=(b);++i)
+#define mp make_pair
+#define tostr to_string
 using namespace std;
 typedef unsigned long long int ull;
 typedef unsigned int ul;
@@ -12,11 +16,27 @@ typedef double ld;
 typedef vector<int> vi;
 typedef long long int ll;
 typedef string str;
+
+// FILE I/O FOR SOME OJ LIKE USACO
+void setIn(str s) { freopen(s.c_str(),"r",stdin); }
+void setOut(str s) { freopen(s.c_str(),"w",stdout); }
+void unsyncIO() { cin.tie(0)->sync_with_stdio(0); }
+void setIO(str s = "") {
+    unsyncIO();
+    // cin.exceptions(cin.failbit);
+    // throws exception when do smth illegal
+    // ex. try to read letter into int
+    if (s.size()) { setIn(s+".in"), setOut(s+".out"); } // for USACO
+}
 const int MOD = 1e9+7;
 const int MX = 2e5+5;
-const ll INF = 1e18;
+const ll LLINF = 1e18;
+const ll INTINF = 1e9;
 const ld PI = acos((ld)-1);
 const int xpianyi[4] = {1,0,-1,0}, ypianyi[4] = {0,1,0,-1}; // for every grid problem!!
+
+
+
 ll cdiv(ll a, ll b) { return a/b+((a^b)>0&&a%b); } // divide a by b rounded up
 ll fdiv(ll a, ll b) { return a/b-((a^b)<0&&a%b); } // divide a by b rounded down
 //For number theory problem
